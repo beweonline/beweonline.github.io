7cc2e69d09f0b6b430fbe4beacca8484c844b421
beweonline <beweonline@yahoo.com>
2023-03-13 22:48:16 +0100

    Update boardJSON.js

--- moesspruino/webIDE/EspruinoTools/plugins/boardJSON.js
+++ moesspruino/webIDE/EspruinoTools/plugins/boardJSON.js
@@ -18,7 +18,7 @@
       name : "Board JSON URL",
       description : "Where to search online for board JSON files - these contain links to up to date firmware as well as detailed information about the board's hardware",
       type : "string",
-      defaultValue : (Espruino.Core.Utils.needsHTTPS()?"https":"http")+"://www.espruino.com/json"
+      defaultValue : (Espruino.Core.Utils.needsHTTPS()?"https":"http")+"://beweonline.github.io/moesspruino/webIDE/json"
     });
     
     
