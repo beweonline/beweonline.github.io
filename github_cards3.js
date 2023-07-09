226d81b94ff5492b0063f40d74db1b2f25705f78
beweonline <beweonline@yahoo.com>
2023-07-10 00:22:55 +0200

    arm1 arm2 video for mobile

--- github_cards3.js
+++ github_cards3.js
@@ -32,16 +32,17 @@ cardsObj3._02 = {
 	swap: [false,false,false],
 	tabs: ["BASE","ARM 1","ARM 2"],
 	titles: [["base assembly >>>","videos/video.html?src=base.mp4"],
-			 ["turntable 1 >>>","videos/turntable.html?src=arm1TEST.mp4"],
+			 ["turntable 1 >>>","videos/turntable.html?src=arm1.mp4"],
 			 ["turntable 2 >>>","videos/turntable.html?src=arm2.mp4"]],
 	hideTitles: true,
 	txtTitle: 'construction support videos',
 	txt: `<p class="flow-text">
+to help with reading the schematic diagrams
 watch a video loop of the base assembly or
 scrub through turntable animations of the second and third stage
 of the moessARM virtual prototype.
-study the upper and lower limbs and their support
-of an articulated pick'n'place robotic arm.
+study the upper and lower limbs of an articulated pick'n'place robotic arm
+and their base support.
 </p>`
 };
 
@@ -55,19 +56,26 @@ cardsObj3._03 = {
 			 ["rocker-coupler >>>","videos/video.html?src=linkage2.mp4"],
 			 ["freedom >>>","videos/video.html?src=linkage3.mp4"]],
 	hideTitles: true,
-	txtTitle: 'construction support videos',
+	txtTitle: 'engineering with four-bars',
 	txt: `<p class="flow-text">
 the mechanics of the arm is describable by a set of planar quadrilateral linkages.
 a common structural base will support one or two pivots to allow a joint to rotate or swing.
 here the pivot is either a servo's pinion where the horn connects or an M4 bolt or
 even a small wooden rod that work as makeshift rotational axes.
-if a bar is limited in it's rotation it's called a rocker.
-through the connection by a coupler the swinging motion of a motor driven rocker
+if a bar is limited in it's rotation it's called a <span class="italic">rocker</span>.
+through the connection by a <span class="italic">coupler</span>
+the swinging motion of a motor driven rocker
 is transfered to another that carries load and acts as a lever.
+<br>
+<br>
 one may ponder optimum dimensions of any four-bar linkage but ultimately
 there is a rather generous degree of tolerance to electro-mechanical design
 and many a variation in the length of rocker and coupler can be compensated by
 coding appropriate servo actions. the convenient application of crimp-on ring terminals
 to establish joints in this project invites to take license with linkage lengths.
+<br>
+<br>
+However, this does not apply at all in the linkage that keeps the gripper parallel
+to the floor. Here rockers and couplers need to form two perfect parallelograms.
 </p>`
 };
