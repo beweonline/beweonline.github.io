d5e69cc9e7361f444f8da8f2f2aab3411565644a
beweonline <beweonline@yahoo.com>
2025-03-23 10:46:33 +0100

    junior code examples update

--- github_cards7.js
+++ github_cards7.js
@@ -3,10 +3,10 @@ var cardsObj7 = {};
 cardsObj7._01 = {
 	id: '01',
 	swap: [false, false],
-	tabs: ["part I mechanics", "plan"],
+	tabs: ["robot construction", "plan"],
 	titles: [["presentation >>>", "cards7/construction/reveal.html"],
 			 ["PDF plan >>>","https://github.com/beweonline/moesspruino/tree/main/plans"]],
-	txtTitle: 'robot construction',
+	txtTitle: 'I mechanics',
 	hideTitles: true,
 	txt: `<p class="flow-text">
 junior is an almost-no-tools-required stripped down version of senior
@@ -28,17 +28,104 @@ href="https://github.com/beweonline/moesspruino/tree/main/plans">plan</a> to be 
 cardsObj7._02 = {
 	id: '02',
 	swap: [false],
-	tabs: ["part II electronics"],
+	tabs: ["control electric flow"],
 	titles: [["breadboard presentation >>>", "cards7/breadboard/reveal.html"]],
-	txtTitle: 'control electric flow',
-	txt: ``
+	txtTitle: 'II electronics',
+	txt: `<p class="flow-text">
+on a half-length 400 pins breadboard, we accomodate the microcontroller, power supply
+with additional wiring and a microswitch button pulled down to ground inline with an indicator LED.
+it's important to note that ordinary jumper wire is a cause of signal instability!
+the ic inside of servo motors responds to elctrical noise and cable movement or slack joints.
+effectively the pulswidth gets longer or shorter resulting in the servo to rotate w/ chaotic jumps.
+much sturdier and thicker wire like a paper clip does the trick, though.
+on ports D6 & D7 such wires stick into the board and female ends of a F2M dupont cable
+attach to it. though it's possible to do the same for the DC+ and DC- end points, 
+the breadboard power supply conveniently offers 5V pins which provide better fastening.
+	`
 };
 
 cardsObj7._03 = {
 	id: '03',
 	swap: [false],
-	tabs: ["part III cybernetics"],
-	titles: [["coding presentation", "cards7/codeJunior/reveal.html"]],
-	txtTitle: 'steering by code',
-	txt: ``
+	tabs: ["elementary cnc", "intermediate cnc"],
+	titles: [["step-by-step A >>>", "cards7/codeJuniorA/reveal.html"],
+	         ["step-by-step B >>>", "cards7/codeJuniorB/reveal.html"]],
+	txtTitle: 'III cybernetics',
+	hideTitles: true,
+	copycode: function(prop){return this['code'+prop]},
+	code1: `\
+sg9 = require('sg91.js');
+myS = ['D6','D7'];
+myO = {vbs: true,
+       inc: 0.1};
+sg9.setup(myS, myO);
+
+\
+`,
+	code2: `\
+cal(1);
+
+ani = function(){
+
+};
+
+\
+`,
+	code3: `\
+cls = NodeMCU.D1;
+pinMode(cls , 'input');
+
+oC = {}
+
+cb = function() {}
+
+setWatch(cb, cls, oC);
+
+ani = function() {}
+
+hld = function() {}
+
+\
+`,
+	txt: `<p class="flow-text">
+espruino firmware w/ its live javascript interpreter makes it fun and intuitive to 
+exercise computer numeric control (cnc). the sg9x libraries provide a simple boilerplate
+mechanism to get going quickly. what's left to do, is: define pins and properties of
+your servos, calibrate by pulsing and define a sequence of servo motions in a reusable function.
+</p>
+<br>
+<p>the 'boilerplate'</p>
+<pre onclick="clipboard(event,['_03','copycode','1'])"><code class="language-javascript">\
+sg9 = require('sg91.js');
+myS = ['D6','D7'];
+myO = {vbs: true,
+       inc: 0.1};
+sg9.setup(myS, myO);
+\</code></pre>
+<br>
+<p>structure for code example A - calibrate by pulse and animate servos</p>
+<pre onclick="clipboard(event,['_03','copycode','2'])"><code class="language-javascript">\
+cal(1);
+ani = function(){};
+\</code></pre>
+<br>
+<p class="flow-text">
+the intermediate example takes it a step further through button control where a
+watchdog observes a pin and an associated callback function decides what to do:
+awake and calibrate the servos, start or pause the servo animation.
+</p>
+<br>
+<p>structure for code exmple B - watchdog, callback & hold function</p>
+<pre onclick="clipboard(event,['_03','copycode','3'])"><code class="language-javascript">\
+cls = NodeMCU.D1;
+pinMode(cls , 'input');
+oC = {}
+cb = function() {}
+
+setWatch(cb, cls, oC);
+
+ani = function() {}
+hld = function() {}
+\</code></pre>
+	`
 };
